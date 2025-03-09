3625076d5e6cb61eb91a4f4746ca0a1756734cd2
Fionn McArdle <f.mcardle1@universityofgalway.ie>
2025-03-09 15:09:47 +0000

    improved mutation

--- operators.py
+++ operators.py
@@ -1,19 +1,39 @@
 import random
 from strategy import Strategy
 
-def mutation(strategy, mutation_rate=0.5):
-    first_move = strategy.first_move if random.random() > mutation_rate else random.choice([0, 1])
+def mutation(strategy, mutation_rate=0.02):
+    if random.random() < mutation_rate:
+        new_first_move = 1 - strategy.first_move  # Flip from 0 to 1 or 1 t to 0
+    else:
+        new_first_move = strategy.first_move
 
-    response_map = {
-        key: val if random.random() > mutation_rate else random.choice([0, 1])
-        for key, val in strategy.responses.items()
-    }
+    if random.random() < mutation_rate:
+        new_hold_grudges = not strategy.hold_grudges
+    else:
+        new_hold_grudges = strategy.hold_grudges
 
-    hold_grudges = strategy.hold_grudges if random.random() > mutation_rate else not strategy.hold_grudges
+    new_random_defection = strategy.random_defection
+    if random.random() < mutation_rate:
+        # Move up/down by 0.1
+        step = random.choice([-0.1, 0.1])
+        new_random_defection = min(max(0.0, new_random_defection + step), 1.0)
 
-    random_defection = strategy.random_defection if random.random() > mutation_rate else random.choice([0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9 ])
+    new_responses = {}
+    for key, old_val in strategy.responses.items():
+        if random.random() < mutation_rate:
+            # Flip from 0 to 1 or 1 to 0
+            new_responses[key] = 1 - old_val
+        else:
+            new_responses[key] = old_val
 
-    return Strategy(name="", first_move=first_move, responses=response_map, memory_size=2, hold_grudges=hold_grudges, random_defection=random_defection)
+    return Strategy(
+        name="",
+        first_move=new_first_move,
+        responses=new_responses,
+        memory_size=strategy.memory_size,
+        hold_grudges=new_hold_grudges,
+        random_defection=new_random_defection
+    )
 
 def crossover(parent1, parent2):
     off_1_first_move,off_2_first_move  = parent2.first_move, parent1.first_move
